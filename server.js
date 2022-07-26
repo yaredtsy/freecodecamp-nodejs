3f7bcd4c55ee7e02ef43ba16eb580a0e8e309d93
yared tsegaye <tsyared@gmail.com>
2022-07-26 10:31:59 +0300

    Log added

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
+app.use(function(req, res){
+  res.send('Hello Express')
+})
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
